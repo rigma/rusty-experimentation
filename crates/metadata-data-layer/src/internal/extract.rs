66b7a6d68a6c8bd2e80d9c8d5c8ea75b0e1f2f8b
rigma <romain.failla@taster.com>
2024-03-14 14:12:37 +0100

    refactor(data-layer): force usage of `std::sync::Arc`

--- crates/metadata-data-layer/src/internal/extract.rs
+++ crates/metadata-data-layer/src/internal/extract.rs
@@ -3,7 +3,7 @@ use async_trait::async_trait;
 use axum_core::extract::{FromRef, FromRequestParts};
 use http::request::Parts;
 use sqlx::postgres::Postgres;
-use std::convert::Infallible;
+use std::{convert::Infallible, sync::Arc};
 
 #[derive(Clone, Debug)]
 pub struct Repository<T>(pub T);
@@ -11,14 +11,14 @@ pub struct Repository<T>(pub T);
 #[async_trait]
 impl<T, S> FromRequestParts<S> for Repository<T>
 where
-    PoolState: FromRef<S>,
+    Arc<PoolState>: FromRef<S>,
     T: repository::Repository<DB = Postgres>,
     S: Send + Sync,
 {
     type Rejection = Infallible;
 
     async fn from_request_parts(_: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool_state = PoolState::from_ref(state);
+        let pool_state = Arc::from_ref(state);
 
         Ok(Self(T::from_pool(pool_state.get_ref())))
     }
