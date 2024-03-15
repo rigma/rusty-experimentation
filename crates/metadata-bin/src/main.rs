7e4f188c545b3abdf7ddb18c926e968fe67a70fe
rigma <romain.failla@taster.com>
2024-03-15 14:17:40 +0100

    feat(bin): assemble rought prototype

--- crates/metadata-bin/src/main.rs
+++ crates/metadata-bin/src/main.rs
@@ -1,21 +1,23 @@
-use axum::{routing::get, Router};
+use metadata_data_layer::PoolState;
+use metadata_http::{init_router, AppState};
+use tokio::net::TcpListener;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
-use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::registry()
         .with(
-            EnvFilter::builder().with_default_directive(LevelFilter::TRACE.into()).from_env_lossy()
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .from_env_lossy(),
         )
-        .with(
-            tracing_subscriber::fmt::layer()
-            .map_event_format(|format| format.json())
-        )
+        .with(tracing_subscriber::fmt::layer().map_event_format(|format| format.json()))
         .init();
 
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+    let state = AppState::new(PoolState::from_env().finalize());
+    let app = init_router(state);
+
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
 
     tracing::info!("Listening on {}", listener.local_addr().unwrap());
--- crates/metadata-http/src/handlers/mod.rs
+++ crates/metadata-http/src/handlers/mod.rs
@@ -6,7 +6,7 @@ mod domains;
 
 pub fn init_router(state: AppState) -> Router {
     Router::new()
-        .route("/domains/:domain_name", get(domains::show))
-        .route("/domains/:domain_name/:block_name", get(blocks::show))
+        .route("/:domain_name", get(domains::show))
+        .route("/:domain_name/:block_name", get(blocks::show))
         .with_state(state)
 }
