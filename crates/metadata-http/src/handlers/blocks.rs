d2cf29a4241338e830d44a4653915b8c88d4739e
rigma <romain.failla@taster.com>
2024-03-20 00:39:57 +0100

    feat(http): implement errors into handlers

--- crates/metadata-http/src/handlers/blocks.rs
+++ crates/metadata-http/src/handlers/blocks.rs
@@ -1,15 +1,49 @@
 use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
 use metadata_data_layer::repositories::BlockRepository;
 use metadata_data_layer_utils::extract::Repository;
+use metadata_http_utils::{HttpError, Problem};
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error)]
+pub enum BlockError {
+    #[error("Block '{0}' is not found.")]
+    NotFoundByName(String),
+}
+
+impl Problem for BlockError {
+    fn ty(&self) -> String {
+        let sub_type = match self {
+            Self::NotFoundByName(_) => "not-found",
+        };
+
+        format!("https://errors.taster.com/metadata/blocks/{sub_type}")
+    }
+
+    fn title(&self) -> String {
+        match self {
+            Self::NotFoundByName(_) => "Block Not Found.".to_string(),
+        }
+    }
+
+    fn detail(&self) -> String {
+        format!("{self}")
+    }
+
+    fn status(&self) -> Option<StatusCode> {
+        match self {
+            Self::NotFoundByName(_) => Some(StatusCode::NOT_FOUND),
+        }
+    }
+}
 
 pub(super) async fn show(
     Path((_, block_name)): Path<(String, String)>,
     Repository(repository): Repository<BlockRepository>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match repository.get_block_by_name(&block_name).await {
-        Ok(Some(block)) => Ok(Json(block)),
-        // TODO(rigma): improve error handling instead of returning raw responses
-        Ok(None) => Err((StatusCode::NOT_FOUND, "Block not found!")),
-        Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "SQL error!")),
+) -> Result<impl IntoResponse, HttpError> {
+    let block = repository.get_block_by_name(&block_name).await?;
+    if let Some(block) = block {
+        Ok(Json(block))
+    } else {
+        Err(BlockError::NotFoundByName(block_name).into())
     }
 }
--- crates/metadata-http/src/handlers/domains.rs
+++ crates/metadata-http/src/handlers/domains.rs
@@ -1,15 +1,49 @@
 use axum::{extract::Path, http::StatusCode, response::IntoResponse, Json};
 use metadata_data_layer::repositories::DomainRepository;
 use metadata_data_layer_utils::extract::Repository;
+use metadata_http_utils::{HttpError, Problem};
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error)]
+enum DomainError {
+    #[error("Domain '{0}' is not found.")]
+    NotFoundByName(String),
+}
+
+impl Problem for DomainError {
+    fn ty(&self) -> String {
+        let sub_type = match self {
+            Self::NotFoundByName(_) => "not-found",
+        };
+
+        format!("https://errors.taster.com/metadata/domains/{sub_type}")
+    }
+
+    fn title(&self) -> String {
+        match self {
+            Self::NotFoundByName(_) => "Domain Not Found.".to_string(),
+        }
+    }
+
+    fn detail(&self) -> String {
+        format!("{self}")
+    }
+
+    fn status(&self) -> Option<StatusCode> {
+        match self {
+            Self::NotFoundByName(_) => Some(StatusCode::NOT_FOUND),
+        }
+    }
+}
 
 pub(super) async fn show(
     Path(domain_name): Path<String>,
     Repository(repository): Repository<DomainRepository>,
-) -> Result<impl IntoResponse, impl IntoResponse> {
-    match repository.get_domain_by_name(&domain_name).await {
-        Ok(Some(domain)) => Ok(Json(domain)),
-        // TODO(rigma): improve error handling instead of returning raw responses
-        Ok(None) => Err((StatusCode::NOT_FOUND, "Domain not found!")),
-        Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "SQL error!")),
+) -> Result<impl IntoResponse, HttpError> {
+    let domain = repository.get_domain_by_name(&domain_name).await?;
+    if let Some(domain) = domain {
+        Ok(Json(domain))
+    } else {
+        Err(DomainError::NotFoundByName(domain_name).into())
     }
 }
