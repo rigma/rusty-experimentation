960954c269c7b03f3813610e39937a28ca60f96f
rigma <romain.failla@taster.com>
2024-03-27 00:29:56 +0100

    feat(bin): add cli option to configure the application

--- crates/metadata-bin/src/main.rs
+++ crates/metadata-bin/src/main.rs
@@ -1,22 +1,14 @@
-mod commands;
+mod cli;
 
 #[cfg(not(debug_assertions))]
 fn main() {
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(commands::serve());
+    cli::main();
 }
 
 #[cfg(debug_assertions)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenvy::dotenv()?;
-    tokio::runtime::Builder::new_current_thread()
-        .enable_all()
-        .build()
-        .unwrap()
-        .block_on(commands::serve());
+    cli::main();
 
     Ok(())
 }
